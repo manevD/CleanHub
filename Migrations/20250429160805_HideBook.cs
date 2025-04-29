1206524748bd9409ff47926062cfe4530583e2ce
Dimitar Manev <dimitar.manev@outlook.de>
2025-04-29 18:08:37 +0200

    add migration HIDE

---
+++ Migrations/20250429160805_HideBook.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class HideBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Hide",
+                table: "Books",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hide",
+                table: "Books");
+        }
+    }
+}
