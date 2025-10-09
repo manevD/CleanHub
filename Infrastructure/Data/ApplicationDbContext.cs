79778720cbe05f2801ca9f4126ef7195f43c192b
Dimitar Manev <dimitar.manev@outlook.de>
2025-10-10 00:53:12 +0200

    Context anpassen

--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -32,26 +32,12 @@ namespace CleanHub.Infrastructure.Data
         //private readonly string _key = "09e88d4fd3c6fa2f9b05a05f166809b7";
         public ApplicationDbContext()
         {
-            //byte[] keyBytes = new byte[16];
-            //byte[] iv = new byte[16];
-            //using (var rng = new RNGCryptoServiceProvider())
-            //{
-            //    rng.GetBytes(iv);
-            //}
-            //_provider = new AesProvider(keyBytes, iv);
         }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
             ChangeTracker.LazyLoadingEnabled = true;
-            //byte[] keyBytes = new byte[16];
-            //byte[] iv = new byte[16];
-            //using (var rng = new RNGCryptoServiceProvider())
-            //{
-            //    rng.GetBytes(iv);
-            //}
-            //_provider = new AesProvider(keyBytes, iv);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -63,82 +49,14 @@ namespace CleanHub.Infrastructure.Data
             // Add configurations for your entities, including primary keys
             modelBuilder.Entity<IdentityUserLogin<string>>().HasKey(l => l.UserId);
             modelBuilder.Entity<Activity>().ToTable("Activity", t => t.ExcludeFromMigrations());
-
-            //  modelBuilder.Entity<Customer>()
-            //   .Property(e => e.CustomerInfo)
-            //   .HasConversion(
-            //       encryptedValue => Encrypt(encryptedValue, _key), // Custom encryption function
-            //       decryptedValue => Decrypt(decryptedValue, _key)  // Custom decryption function
-            //   );
-            //  modelBuilder.Entity<Customer>()
-            //  .Property(e => e.Email)
-            //  .HasConversion(
-            //      encryptedValue => Encrypt(encryptedValue, _key), // Custom encryption function
-            //      decryptedValue => Decrypt(decryptedValue, _key)  // Custom decryption function
-            //  );
-            //  modelBuilder.Entity<Customer>()
-            //.Property(e => e.Web)
-            //.HasConversion(
-            //    encryptedValue => Encrypt(encryptedValue, _key), // Custom encryption function
-            //    decryptedValue => Decrypt(decryptedValue, _key)  // Custom decryption function
-            //);
-            //      modelBuilder.Entity<Customer>()
-            // .Property(e => e.PhoneNumber)
-            // .HasConversion(
-            //     encryptedValue => Encrypt(encryptedValue, _key), // Custom encryption function
-            //     decryptedValue => Decrypt(decryptedValue, _key)  // Custom decryption function
-            // );
         }
 
-        static string Encrypt(string data, string key)
-        {
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = Encoding.UTF8.GetBytes(key);
-                aesAlg.IV = new byte[16]; // You might want to generate a random IV for each encryption in a real-world scenario
-
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-                using (var msEncrypt = new System.IO.MemoryStream())
-                {
-                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (var swEncrypt = new System.IO.StreamWriter(csEncrypt))
-                        {
-                            swEncrypt.Write(data);
-                        }
-                    }
-                    return Convert.ToBase64String(msEncrypt.ToArray());
-                }
-            }
-        }
-
-        static string Decrypt(string encryptedData, string key)
-        {
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = Encoding.UTF8.GetBytes(key);
-                aesAlg.IV = new byte[16]; // You might want to use the same IV that was used for encryption
-
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-
-                using (var msDecrypt = new System.IO.MemoryStream(Convert.FromBase64String(encryptedData)))
-                {
-                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                    {
-                        using (var srDecrypt = new System.IO.StreamReader(csDecrypt))
-                        {
-                            return srDecrypt.ReadToEnd();
-                        }
-                    }
-                }
-            }
-        }
-     //   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-     //    => optionsBuilder.UseSqlServer(
-     //"Data Source=SQL6032.site4now.net;Initial Catalog=db_aae56c_2025martitest;User Id=db_aae56c_2025martitest_admin;Password=Hallo123!");
+       
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseSqlServer(
-        "Server=.\\SQLEXPRESS;Database=2025MartiTestMai;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;");
+         => optionsBuilder.UseSqlServer(
+     "Data Source=SQL6032.site4now.net;Initial Catalog=db_aae56c_2025martitest;User Id=db_aae56c_2025martitest_admin;Password=Hallo123!");
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //    => optionsBuilder.UseSqlServer(
+        //"Server=.\\SQLEXPRESS;Database=2025MartiTestMai;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;");
     }
 }
